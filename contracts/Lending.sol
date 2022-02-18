d49adc2be8ee8c54175d1b66a3f0875729afc7c8
Ramdhani <dhani.rachmansyah@georgebrown.ca>
2022-02-17 20:32:16 -0500

    feat: borrower deposit

--- contracts/Lending.sol
+++ contracts/Lending.sol
@@ -1,22 +1,49 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 contract Lending {
-    string private greeting;
+    //#region State variables
+    mapping(address => bool) private owners;
+    mapping(address => uint256) private deposits;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Lending with greeting:", _greeting);
-        greeting = _greeting;
+    //#region
+
+    constructor() {
+        // TODO this should be replaced with our address
+        // so that we're added as the contract owners (TBD)
+        owners[address(uint160(bytes20("0x1")))] = true;
+        owners[address(uint160(bytes20("0x2")))] = true;
+        owners[address(uint160(bytes20("0x3")))] = true;
+        owners[address(uint160(bytes20("0x4")))] = true;
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    //#region Modifiers
+    modifier onlyOwners() {
+        require(owners[msg.sender], "Only for owners!");
+        _;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    //#region
+
+    // we use external to save gas because we know this function can only be called externally
+    function depositMoney() external payable {
+        require(msg.value > 0, "Deposit must be greater than 0!");
+        require(msg.sender.balance >= msg.value, "Not enough funds!");
+
+        deposits[msg.sender] += msg.value;
+    }
+
+    function getDepositBalance() external view returns (uint256) {
+        return deposits[msg.sender];
+    }
+
+    function withdrawDeposit() external payable {
+        require(deposits[msg.sender] > 0, "You don't have any deposit!");
+
+        uint256 _amount = deposits[msg.sender];
+        deposits[msg.sender] = 0;
+
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to withdraw");
     }
 }
