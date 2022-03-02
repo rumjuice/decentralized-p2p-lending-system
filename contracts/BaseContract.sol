37bcc0ed3174614d3d23e7fca320ef493666dc62
Ramdhani Haris <ramdhani@live.com>
2022-03-01 22:07:01 -0500

    Restructure (#16)
    * utils: cleanup and add soldocs
    
    * base: cleanup and add soldocs
    
    * lending: restructure and add soldocs
    
    Co-authored-by: Ramdhani <dhani.rachmansyah@georgebrown.ca>

--- contracts/BaseContract.sol
+++ contracts/BaseContract.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "./Context.sol";
+
+/// @notice Error declarations
 error NotRegisteredOwner();
 error NotRegisteredBorrower();
 error NotRegisteredLender();
@@ -12,22 +14,52 @@ error HasActiveLoan();
 error OnlyOwnersAndBorrowersCanAccess();
 error InvalidInterestRate();
 error OnlyOwnersAndLendersCanAccess();
-contract BaseContract is Context {
-    mapping(address => bool)  owners;
-    mapping(address => bool)  borrowers;
-    mapping(address => bool)  lenders;
-    mapping(address => uint256)  deposits;
-    mapping(address => bool)  mutex;
-    mapping(address => uint256)  borrowerLoanRequest;
-    // mapping of borrower address to loan request index
-    // mapping(address => uint256) private borrowerLoanRequest;
-    mapping(address => uint256)  lendersInvestment;
-    // mapping(address => bool) private lenders;
-    // mapping of borrower address to credit score
-    mapping(address => uint8)  borrowerCreditScores;
 
-        constructor() {
-        // Owners contract address
+/// @title Base Contract
+/// @author Rav, Jainam, Dhani, Hossein
+/// @notice High-level contract to store information
+/// @dev Interface of the main contract so that the code is clean, maintainable, and reusable
+contract BaseContract is Context {
+    /// @notice Contract owners
+    mapping(address => bool) private owners;
+    /// @notice Store list of borrowers
+    mapping(address => bool) internal borrowers;
+    /// @notice Store list of lenders
+    mapping(address => bool) internal lenders;
+    /// @notice Store borrower's deposit
+    mapping(address => uint256) internal deposits;
+    /// @notice Store index of borrower's loan request
+    mapping(address => uint256) internal borrowerLoanRequest;
+    /// @notice Store borrower's credit score
+    mapping(address => uint8) internal borrowerCreditScore;
+    /// @dev Utility variable to prevent recursion
+    mapping(address => bool) private mutex;
+    /// @notice Store balance of smart contract (profit)
+    uint256 internal contractProfit;
+    /// @notice Store list of loan request
+    LoanRequest[] internal loanRequests;
+
+    /// @notice Loan status
+    enum LoanStatus {
+        NEW,
+        ON_LOAN,
+        PAID,
+        CANCELED,
+        CLOSED_BY_LENDER
+    }
+
+    /// @notice Loan request object
+    struct LoanRequest {
+        address lender;
+        address borrower;
+        uint256 amount;
+        uint8 interestRate;
+        uint8 creditScore;
+        LoanStatus status;
+    }
+
+    constructor() {
+        /// @notice Initialize owners contract address
         // Ramdhani address
         owners[address(0x9321ef8Ccf26Ca4d64F7213076B3BAb0F6253E96)] = true;
         // Jainam address
@@ -36,65 +68,73 @@ contract BaseContract is Context {
         owners[address(0xf859ECf4Ea6322F706F908aAA76702c3CA7faEbB)] = true;
         // Ravshan address
         owners[address(0x42Bd936410fE89CFfB8cbb3934A6FD3D6F76cB2a)] = true;
-
-        // TODO remove this when we're finished
-        owners[msg.sender] = true;
-
     }
 
-     modifier onlyOwners() {
+    /// @notice Only owners can execute
+    modifier onlyOwners() {
         if (!owners[msg.sender]) revert NotRegisteredOwner();
         _;
     }
 
+    /// @notice Only borrowers can execute
     modifier onlyBorrowers() {
         if (!borrowers[msg.sender]) revert NotRegisteredBorrower();
         _;
     }
 
+    /// @notice Only lenders can execute
+    modifier onlyLenders() {
+        if (!lenders[msg.sender]) revert NotRegisteredLender();
+        _;
+    }
+
+    /// @notice Borrowers can't execute
     modifier notBorrowers() {
         if (!(owners[msg.sender] || lenders[msg.sender]))
             revert OnlyOwnersAndBorrowersCanAccess();
         _;
     }
 
+    /// @notice Lenders can't execute
     modifier notLenders() {
         if (!(owners[msg.sender] || borrowers[msg.sender]))
             revert OnlyOwnersAndLendersCanAccess();
         _;
     }
 
-    modifier onlyLenders() {
-        if (!lenders[msg.sender]) revert NotRegisteredLender();
-        _;
-    }
-
+    /// @notice Amount must be more than 0
     modifier isValidAmountSent() {
         if (msg.value <= 0) revert DepositCannotBeZero();
         _;
     }
 
+    /// @notice Ensure caller have enough balance
     modifier hasEnoughBalance() {
         if (msg.sender.balance < msg.value) revert NotEnoughFunds();
         _;
     }
 
+    /// @notice Ensure address is valid
     modifier isValidAddress(address _addr) {
         if (_addr == 0x0000000000000000000000000000000000000000)
             revert BurnAddressProhibited();
         _;
     }
 
+    /// @notice Ensure deposit is not 0
     modifier hasNotZeroDepositBalance() {
         if (deposits[msg.sender] <= 0) revert NoFundsInDeposit();
         _;
     }
-    // modifier to freeze deposit
+
+    /// @notice Ensure borrowers doesn't have active loan
     modifier hasNoActiveLoan() {
         if (borrowerLoanRequest[msg.sender] > 0) revert HasActiveLoan();
         _;
     }
 
+    /// @notice Prevent multiple calls to the function
+    /// @dev Prevent function recursion
     modifier preventRecursion() {
         if (!mutex[msg.sender]) {
             mutex[msg.sender] = true;
@@ -103,9 +143,9 @@ contract BaseContract is Context {
         mutex[msg.sender] = false;
     }
 
-    modifier isValidIntererstRate(uint8 _interestRate) {
+    /// @notice Ensure interest is not more than 100
+    modifier isValidInterestRate(uint8 _interestRate) {
         if (_interestRate > 100) revert InvalidInterestRate();
         _;
     }
-
 }
--- contracts/Lending.sol
+++ contracts/Lending.sol
@@ -4,47 +4,26 @@ pragma solidity ^0.8.0;
 import "./Utils.sol";
 import "./BaseContract.sol";
 
-contract Lending is BaseContract{
-    //#region State variables
+/// @title Main lending Contract
+/// @author Rav, Jainam, Dhani, Hossein
+/// @notice P2P lending smart contract
+/// @dev P2P lending smart contract
+contract Lending is BaseContract {
     uint8 public interestRate;
-    // Keep balance amount of smart contract (profit)
-    uint256 private balanceAmountOfSmartContractProfit;
-    enum LoanStatus {
-        NEW,
-        ON_LOAN,
-        PAID,
-        CANCELED,
-        CLOSED_BY_LENDER
-    }
-    struct LoanRequest {
-        address lender;
-        address borrower;
-        uint256 amount;
-        uint8 interestRate;
-        uint8 creditScore;
-        LoanStatus status;
-    }
-
-    // list of loan request
-    LoanRequest[] private loanRequests;
- 
-
-    //#region
 
     constructor() {
-        // default interestRate
         interestRate = 10;
-        // put dummy loan request for index 0
-        // because in borrowerLoanRequest mapping, it will return 0 if the address is not found
-        loanRequests.push(LoanRequest(address(0), address(0), 0, 0, 0, LoanStatus.NEW));
+        /// @notice Dummy loan request for index 0
+        /// @dev because in borrowerLoanRequest mapping, it will return 0 if the address is not found
+        loanRequests.push(
+            LoanRequest(address(0), address(0), 0, 0, 0, LoanStatus.NEW)
+        );
     }
 
- 
-    //#region
+    /// @dev we use external visibility to save gas because we know some of these functions can only be called externally
 
-    // we use external to save gas because we know these functions can only be called externally
-
-    //owners should register borrowers
+    /// @notice Owner can register borrower
+    /// @param _newBorrower (address) borower address
     function registerBorrower(address _newBorrower)
         external
         onlyOwners
@@ -53,7 +32,8 @@ contract Lending is BaseContract{
         borrowers[_newBorrower] = true;
     }
 
-    //owners should unregister not needed borrowers
+    /// @notice Owner can unregister borrower
+    /// @param _removeBorrower (address) borrower address
     function unregisterBorrower(address _removeBorrower)
         external
         onlyOwners
@@ -62,6 +42,28 @@ contract Lending is BaseContract{
         borrowers[_removeBorrower] = false;
     }
 
+    /// @notice Owner can register lenders
+    /// @param _newLender (address) lender address
+    function registerLender(address _newLender)
+        external
+        onlyOwners
+        isValidAddress(_newLender)
+    {
+        lenders[_newLender] = true;
+    }
+
+    /// @notice Owner can unregister lenders
+    /// @param _removeLender (address) lender address
+    function unregisterLender(address _removeLender)
+        external
+        onlyOwners
+        isValidAddress(_removeLender)
+    {
+        borrowers[_removeLender] = false;
+    }
+
+    /// @notice Borrower can deposit money
+    /// @dev Payable function to receive deposit from borrower
     function depositMoney()
         external
         payable
@@ -70,14 +72,22 @@ contract Lending is BaseContract{
         hasEnoughBalance
         hasNoActiveLoan
     {
-        require(msg.value <= (borrowerCreditScores[msg.sender] + 1) * 1 ether, "Your deposit must equal or less than (your credit score + 1) * 1 ether");
-            deposits[msg.sender] += msg.value;
+        /// @notice Deposit must be less than credit score limit
+        require(
+            msg.value <= (borrowerCreditScore[msg.sender] + 1) * 1 ether,
+            "Your deposit must equal or less than (your credit score + 1) * 1 ether"
+        );
+        deposits[msg.sender] += msg.value;
     }
 
+    /// @notice Get borrower's deposit balance
+    /// @return (uint256)
     function getDepositBalance() external view onlyBorrowers returns (uint256) {
         return deposits[msg.sender];
     }
 
+    /// @notice Borrower can withdraw their deposit
+    /// @dev If borrower has deposit and has no active loan
     function withdrawDeposit()
         external
         onlyBorrowers
@@ -92,159 +102,51 @@ contract Lending is BaseContract{
         require(sent, "Failed to withdraw");
     }
 
+    /// @notice Borrower can request a loan based on their credit score
     function requestLoan()
         external
         onlyBorrowers
         hasNotZeroDepositBalance
         hasNoActiveLoan
     {
-        // Checking the credit score to define the max amount for the deposit.
-        // ***
-        // Instead of giving power of unlimited depositing to borrowers,
-        // we can extend credit levels up to 10 or more.
-        // ***
+        /// @notice Checking the credit score to define the max amount that can be lent
+        // instead of giving power of unlimited depositing to borrowers,
+        /// @dev we can extend credit levels up to 10 or more.
         // 0 level credit score limit is 1 Eth.
         // 1 level credit score limit is 2 Eth.
         // 2 level credit score limit is 3 Eth.
         uint256 _maxAmount;
-        uint8 creditlevel = borrowerCreditScores[msg.sender];
-        if (creditlevel==0) {
-            _maxAmount= 1;
-        } else if (creditlevel==1) {
-            _maxAmount= 2;
-        } else if (creditlevel==2) {
-            _maxAmount= 3;
-        }         
+        uint8 _creditScore = borrowerCreditScore[msg.sender];
+        if (_creditScore == 0) {
+            _maxAmount = 1;
+        } else if (_creditScore == 1) {
+            _maxAmount = 2;
+        } else if (_creditScore == 2) {
+            _maxAmount = 3;
+        }
 
-        // create loan object
+        /// @dev Push loan request object to loan requests array
+        // initialize lender with 0 address
         loanRequests.push(
             LoanRequest({
                 lender: address(0),
-                borrower: msg.sender, 
+                borrower: msg.sender,
                 amount: _maxAmount,
                 interestRate: interestRate,
-                creditScore: borrowerCreditScores[msg.sender],
+                creditScore: borrowerCreditScore[msg.sender],
                 status: LoanStatus.NEW
             })
         );
-        // get index
+        /// @dev Get the array index
         uint256 _index = loanRequests.length - 1;
-        // put into borrower loan mapping
+        /// @dev Put the index into borrower loan mapping
         borrowerLoanRequest[msg.sender] = _index;
 
-        //freeze deposit amount(50% of loan request)
-        //subtract returned amount from deposit balance of borrower
+        /// @notice freeze deposit amount (50% of loan request)
+        /// @dev subtract returned amount from deposit balance of borrower
         deposits[msg.sender] -= _maxAmount;
     }
 
-    function getLoanStatus(address _borrower)
-        external
-        view
-        notLenders
-        returns (string memory)
-    {
-        uint256 _index = borrowerLoanRequest[_borrower];
-        require(_index > 0, "You have no active loan");
-
-        return Utils.getStatus(uint8(loanRequests[_index].status));
-    }
-
-    //get smart contract balance
-    function balanceOfContract() external view onlyOwners returns (uint256) {
-        return address(this).balance;
-    }
-
-    //owners should register lenders
-    function registerLender(address _newLender)
-        external
-        onlyOwners
-        isValidAddress(_newLender)
-    {
-        lenders[_newLender] = true;
-    }
-
-    //owners should unregister lender
-    function unregisterLender(address _removeLender)
-        external
-        onlyOwners
-        isValidAddress(_removeLender)
-    {
-        borrowers[_removeLender] = false;
-    }
-
-    //take transaction fee which is 1% borrowed amount
-    //whenever a borrower pays back debt, his address and amount needs to be passed to this method
-    function takeProcessingFee(address _borrower, uint256 _borrowedAmount)
-        internal
-    {
-        uint256 borrowerDeposit = deposits[_borrower];
-        uint256 fee = Utils.percentage(_borrowedAmount, 1);
-        if (fee > borrowerDeposit) {
-            balanceAmountOfSmartContractProfit += borrowerDeposit;
-            deposits[_borrower] = 0;
-        } else {
-            deposits[_borrower] = borrowerDeposit - fee;
-            balanceAmountOfSmartContractProfit += fee;
-        }
-    }
-
-    // Finding the borrower amount field in the loan request array through their address.
-    function findBorrowerAmountInLoanRequests() internal view returns (uint256) {
-        return loanRequests[borrowerLoanRequest[msg.sender]].amount;
-        // description:
-        //uint256 _index = borrowerLoanRequest[msg.sender];
-        //uint256 _amount = loanRequests[_index].amount;
-        //return _amount;
-    }
-
-    //*** The borrower pays the debt ***
-    function borrowerPaysDebt() external payable onlyBorrowers returns (bool){
-        // Checking if the borrower has an active loan (status == ON_LOAN)
-        uint _index=borrowerLoanRequest[msg.sender];
-        require(loanRequests[_index].status == LoanStatus.ON_LOAN , "You have no active loan");
-        uint256 debtAmount = findBorrowerAmountInLoanRequests();  
-        uint256 returnPaybackAmount = debtAmount + ((debtAmount * interestRate) / 100);
-        // Check if the value sent by the borrower is equal to their (his/her) loan amount.
-        require(msg.value == returnPaybackAmount * 1 ether, "Your payback amount is not correct!");
-        // Finding the lender address associated with this borrower
-        // in order to pay the debt to the lender 
-        address payable LenderAddressAssociatedWithThisBorrower = payable(loanRequests[borrowerLoanRequest[msg.sender]].lender);
-        // Paying back to the lender.
-        LenderAddressAssociatedWithThisBorrower.transfer(returnPaybackAmount * 1 ether);
-        // Updating loan status to PAID.
-        loanRequests[_index].status == LoanStatus.PAID;
-        //Processing of taking the fee amount that will be added to the smart contract balance
-        //and will be deducted from the borrower deposit.       
-         takeProcessingFee(msg.sender, debtAmount);
-         if (borrowerCreditScores[msg.sender]<2){
-                borrowerCreditScores[msg.sender]++;
-         }
-         return true;
-    }
-
-
-    //*** Lending function ***
-    //The lender selects one of the loan requests,
-    //and transfers the requested amount to the borrower.
-    //Then,The required updates will be applied.
-    function lending(uint256 _index) external payable onlyLenders returns (bool ) {
-        //Verifying if the selected loan request is valid.
-        require(loanRequests[_index].status == LoanStatus.NEW , "Invalid index");
-        //Verifying the transferred value of the lender according to the borrower's requested loan amount 
-        //(should be equal).
-        require(msg.value == loanRequests[_index].amount * 1 ether, "The amount is not correct!");
-        //Applying the required updates.
-        loanRequests[_index].lender = msg.sender;
-        loanRequests[_index].amount = msg.value;
-        loanRequests[_index].status = LoanStatus.ON_LOAN;
-        //Transferring the requested amount to the borrower by the lender.
-        address payable barrowerAddress = payable(loanRequests[_index].borrower);
-        barrowerAddress.transfer(msg.value * 1 ether);
-        return true;
-    }
-    
-
-    //owners and lenders can access the loanRequests list
     function getLoanList()
         external
         view
@@ -254,47 +156,169 @@ contract Lending is BaseContract{
         return loanRequests;
     }
 
-    //owners can set interest rate
+    /// @notice Function for lenders to provide lending
+    /// @dev The lender selects one of the loan requests and transfers the requested amount to the borrower
+    /// @param _index (uint256) loan request index
+    function lending(uint256 _index) external payable onlyLenders {
+        /// @dev Verify if the selected loan request is valid
+        require(_index > 0 && loanRequests[_index].amount > 0, "Invalid loan");
+        /// @dev Check if the loan status is new
+        require(
+            loanRequests[_index].status == LoanStatus.NEW,
+            "Loan is not open"
+        );
+        /// @dev Verifying the transferred value of the lender according to the borrower's requested loan amount
+        require(
+            msg.value == loanRequests[_index].amount * 1 ether,
+            "Incorrect amount"
+        );
+        /// @dev Update the loan object
+        loanRequests[_index].lender = msg.sender;
+        loanRequests[_index].status = LoanStatus.ON_LOAN;
+        /// @dev Send the requested amount to the borrower
+        (bool sent, ) = loanRequests[_index].borrower.call{
+            value: msg.value * 1 ether
+        }("");
+        require(sent, "Failed to lend");
+    }
+
+    /// @notice Get the amount borrowed
+    /// @return (uint256)
+    function findBorrowerAmountInLoanRequests()
+        internal
+        view
+        returns (uint256)
+    {
+        return loanRequests[borrowerLoanRequest[msg.sender]].amount;
+    }
+
+    /// @notice Payout function for borrower to pay the debt
+    function payout() external payable onlyBorrowers {
+        /// @dev Checking if borrower has active loan
+        uint256 _index = borrowerLoanRequest[msg.sender];
+        require(
+            loanRequests[_index].status == LoanStatus.ON_LOAN,
+            "You have no active loan"
+        );
+        /// @dev Calculate the payback amount (principal + interest)
+        uint256 _debtAmount = findBorrowerAmountInLoanRequests();
+        uint256 _returnPaybackAmount = _debtAmount +
+            ((_debtAmount * interestRate) / 100);
+        /// @dev Check if the value sent by the borrower is equal to their loan amount
+        require(
+            msg.value == _returnPaybackAmount * 1 ether,
+            "Amount is not correct!"
+        );
+        /// @dev Get the lender address
+        address payable _lenderAddress = payable(
+            loanRequests[borrowerLoanRequest[msg.sender]].lender
+        );
+        /// @dev Updating loan status to PAID
+        loanRequests[_index].status == LoanStatus.PAID;
+        /// @dev Paying back to the lender
+        (bool sent, ) = _lenderAddress.call{
+            value: _returnPaybackAmount * 1 ether
+        }("");
+        require(sent, "Failed to pay");
+        /// @dev Take 1% processing fee for this contract profit, deducted from borrower's deposit
+        takeProcessingFee(msg.sender, _debtAmount);
+        /// @dev Increment borrower's credit score
+        if (borrowerCreditScore[msg.sender] < 2) {
+            borrowerCreditScore[msg.sender]++;
+        }
+    }
+
+    /// @notice Get loan status
+    /// @param _borrower (address) borrower address
+    /// @return (string)
+    function getLoanStatus(address _borrower)
+        external
+        view
+        notLenders
+        returns (string memory)
+    {
+        uint256 _index = borrowerLoanRequest[_borrower];
+        require(_index > 0, "You have no active loan");
+
+        return Utils.getStatus(loanRequests[_index].status);
+    }
+
+    /// @notice Get smart contract balance
+    /// @return (uint256)
+    function balanceOfContract() external view onlyOwners returns (uint256) {
+        return address(this).balance;
+    }
+
+    /// @notice Take transaction fee which is 1% of borrowed amount
+    /// @dev Whenever a borrower pays back debt, their address and amount needs to be passed to this method
+    /// @param _borrower (address) borrower address
+    /// @param _borrowedAmount (uint256) borrowed amount
+    function takeProcessingFee(address _borrower, uint256 _borrowedAmount)
+        internal
+    {
+        uint256 _borrowerDeposit = deposits[_borrower];
+        uint256 _fee = Utils.percentage(_borrowedAmount, 1);
+        if (_fee > _borrowerDeposit) {
+            contractProfit += _borrowerDeposit;
+            deposits[_borrower] = 0;
+        } else {
+            deposits[_borrower] = _borrowerDeposit - _fee;
+            contractProfit += _fee;
+        }
+    }
+
+    /// @notice Owner can set interest rate
+    /// @param _interestRate (uint8)
     function setInterestRate(uint8 _interestRate)
         external
-        isValidIntererstRate(_interestRate)
+        isValidInterestRate(_interestRate)
         onlyOwners
     {
         interestRate = _interestRate;
     }
 
-    function withdrawAll() public payable onlyOwners {
-        payable(msg.sender).transfer(this.balanceOfContract());
-
+    /// @notice Owner can withdraw smart contract balance
+    function withdrawAll() external onlyOwners {
+        (bool sent, ) = msg.sender.call{value: this.balanceOfContract()}("");
+        require(sent, "Failed to withdraw");
     }
-    function forceCloseLoanRequest(address borrowerAddress)
+
+    /// @notice Lender can force close their loan
+    /// @dev In case the borrower is unable to pay back, lender will get all the borrower's deposit
+    /// @param _borrowerAddress (address)
+    function forceCloseLoanRequest(address _borrowerAddress)
         external
         onlyLenders
     {
-        //find borrower
-        uint256 loanIndex =  borrowerLoanRequest[borrowerAddress];
+        /// @dev Get loan request index
+        uint256 _loanIndex = borrowerLoanRequest[_borrowerAddress];
 
-        //check if loan request exists with provided borrower address
+        /// @dev Check if loan exist
+        require(_loanIndex > 0, "Loan not found");
+
+        /// @dev Check if lender has active loan with given borrower
         require(
-            loanIndex != 0, 
-            "No loan request associated with this address");
+            loanRequests[_loanIndex].status == LoanStatus.ON_LOAN &&
+                loanRequests[_loanIndex].lender == msg.sender,
+            "You have no loan payout"
+        );
 
-        //check if lender has active loan with given borrower
-        require(
-            loanRequests[loanIndex].status == LoanStatus.ON_LOAN 
-            && loanRequests[loanIndex].lender == msg.sender, 
-            "You have no loan payout");
+        /// @dev change the status of the loan request
+        loanRequests[_loanIndex].status = LoanStatus.CLOSED_BY_LENDER;
 
-        //change the status of the loan request
-        loanRequests[loanIndex].status == LoanStatus.CLOSED_BY_LENDER;
+        /// @dev change borrower credit score to 0 for not returning loan
+        borrowerCreditScore[_borrowerAddress] = 0;
 
-        //change borrower credit score to 0 for not returning loan 
-        borrowerCreditScores[borrowerAddress] = 0;
+        /// @dev 50% of loan amount should be returned
+        uint256 amount = (loanRequests[_loanIndex].amount / 2);
 
-        //50% of loan amount should be returned
-        uint256 amount = (loanRequests[loanIndex].amount/2);
-        
-        //send the money to lender
+        /// @dev Empty borrower's deposit
+        deposits[_borrowerAddress] = 0;
+
+        /// @dev Remove borrower loan request
+        delete borrowerLoanRequest[_borrowerAddress];
+
+        /// @dev send the money to lender
         (bool sent, ) = msg.sender.call{value: amount}("");
         require(sent, "Failed to withdraw");
     }
