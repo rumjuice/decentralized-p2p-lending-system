e5876daec6a169e944f934c494bedfccfb184921
Ramdhani <dhani.rachmansyah@georgebrown.ca>
2022-03-01 19:26:31 -0500

    utils: cleanup and add soldocs

--- contracts/Utils.sol
+++ contracts/Utils.sol
@@ -1,18 +1,40 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "./BaseContract.sol";
 
+/// @title Utility contract
+/// @author Rav, Jainam, Dhani, Hossein
+/// @notice Provides utility function
+/// @dev Pure function for calculation and status mapping
 library Utils {
-    function percentage(uint256 _amount, uint8 _rate)
+    /// @notice Calculate percentage of given amount
+    /// @dev Calculate percentage of given amount
+    /// @param _amount (uint256)
+    /// @param _percentage (uint8)
+    /// @return  (uint256)
+    function percentage(uint256 _amount, uint8 _percentage)
         internal
         pure
         returns (uint256)
     {
         // Solidity > 0.8 already have built-in overflow checking
-        return (_amount * _rate) / 100;
+        return (_amount * _percentage) / 100;
     }
 
-    // Get loan status in string
-    function getStatus(uint8 _status) internal pure returns (string memory) {
-        return _status == 0 ? "NEW" : _status == 1 ? "ON_LOAN" : "PAID";
+    /// @notice Get status in text
+    /// @dev Get status in string based on LoanStatus enum
+    /// @param _status (LoanStatus)
+    /// @return _statusName (string)
+    function getStatus(BaseContract.LoanStatus _status)
+        internal
+        pure
+        returns (string memory _statusName)
+    {
+        if (_status == BaseContract.LoanStatus.NEW) return "NEW";
+        if (_status == BaseContract.LoanStatus.ON_LOAN) return "ON_LOAN";
+        if (_status == BaseContract.LoanStatus.PAID) return "PAID";
+        if (_status == BaseContract.LoanStatus.CANCELED) return "CANCELED";
+        if (_status == BaseContract.LoanStatus.CLOSED_BY_LENDER)
+            return "CLOSED_BY_LENDER";
     }
 }
